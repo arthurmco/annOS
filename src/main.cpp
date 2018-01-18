716be1e912ca7b8faaac1dfa7c1ad44f596b7776
Arthur Mendes <arthurmco@gmail.com>
2018-01-18 01:43:35 -0200

    Import multiboot information structure in our kernel
    This structure have some very important blocks, like the memory map and
    memory total, who will be very important to our physical memory manager.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <VGAConsole.hpp>
+#include <DebugConsole.hpp>
 #include <Log.hpp>
 #include <arch/x86/IO.hpp>
 #include <arch/x86/IDT.hpp>
@@ -17,16 +18,69 @@ extern "C" void __cxa_pure_virtual()
     panic("called virtual function without body");
 }
 
-extern "C"
-void kernel_main(void) {
+/**
+ * Boot structure
+ */
+struct BootStruct {
+    /**
+     * The multiboot magic number
+     */
+    uint32_t magic;
 
+    /**
+     * Physical memory pointer for the multiboot boot information structure 
+     */
+    uintptr_t multiboot_phys_ptr;
+} __attribute((packed));
+
+/**
+ * Multiboot Boot Information Format structure
+ */
+struct MultibootBIF {
+    // Flags used what the bootloader supports
+    uint32_t flags;
+
+    uint32_t mem_lower; // Lower memory count
+    uint32_t mem_upper; // Upper memoty count
+
+    // Boot device
+    uint32_t boot_device;
+
+    // Boot loader command line
+    uint32_t cmdline;
+
+    // Multiboot modules information
+    uint32_t mods_count;
+    uint32_t mods_addr;
+
+    // Executable symbols
+    struct {
+	uint32_t num, size, addr, shndx;
+    } elf_sym;
+
+    // Memory map information
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+};
+
+extern "C"
+void kernel_main(BootStruct* bs) {
     VGAConsole v;
     v.Clear();
     v.WriteVGA("annos v0.0.1\n", BaseColors::LightBlue);
     v.WriteVGA("Copyright (C) 2018 Arthur M\n");
 
     init_stdio(&v);
-    Log::Init(&v);
+
+    SerialLogConsole dc;
+    Log::Init(&dc);
+
+    if (bs->magic != 0x2badb002) {
+	Log::Write(LogLevel::Error, "boot",
+		   "Multiboot magic number is wrong: 0x%08x != 0x2badb002",
+		   bs->magic);
+	panic("invalid boot magic number");
+    }
     
     ::x86::IDT idt;
     idt.Register();
@@ -41,10 +95,21 @@ void kernel_main(void) {
 
     ::x86::IRQHandler::Init(&idt, &oi8259);
     kprintf("...irqhandler\n ");
-
-    Log::Write(LogLevel::Error, "test", "Notice");
     
     asm volatile("sti");
+    
+    kprintf("\n Magic value is 0x%08x, multiboot is at address 0x%x\n",
+	    bs->magic, bs->multiboot_phys_ptr);
+
+    MultibootBIF* bif = (MultibootBIF*)bs->multiboot_phys_ptr;
+    kprintf("\t -> Bootloader flags: \033[1m0x%08x\033[0m\n", bif->flags);
+    kprintf("\t -> Memory total: \033[1m%d kB\033[0m\n",
+	    bif->mem_lower+bif->mem_upper);
+    kprintf("\t -> Boot device: \033[1m %x \033[0m\n",
+	    bif->boot_device);
+    kprintf("\t -> Command line: \033[1m%s\033[0m\n",
+	    ((const char*)bif->cmdline));
+	    
     
     for (;;) {
 	asm volatile("hlt");
